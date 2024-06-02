4eb3113a7bf3d095fbd5f84d33b4b9fb5fac98a8
GOKULNATH K <k.gokulnath1631@gmail.com>
2024-06-02 18:15:13 +0530

    added price update controller

--- backend/app.js
+++ backend/app.js
@@ -41,10 +41,12 @@ app.use(cors(corsOptions));
 const products = require('./routes/product')
 const user = require('./routes/user');
 const order = require('./routes/order');
+const itemsRouter = require('./routes/item');
 
 app.use('/api/v1', products);
 app.use('/api/v1',user);
 app.use('/api/v1',order);
+app.use('/api/v1', itemsRouter);
 
 
 
